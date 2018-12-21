abc7983c51932424d6b0f77ab779c9679e0f0c3b
Curiosity1337 <axaxaxalol@gmail.com>
2018-12-21 16:55:12 +0300

    1

--- js/script.js
+++ js/script.js
@@ -8,10 +8,10 @@ $(document).ready(function () {
     });
     $('.prices__button').click(function () {
         $('.layout').fadeIn();
-        $('body').addClass("noscroll")
+        $('html').addClass("noscroll")
     });
     $('.close').click(function () {
         $('.layout').fadeOut();
-        $('body').removeClass("noscroll")
+        $('html').removeClass("noscroll")
     });
 });
